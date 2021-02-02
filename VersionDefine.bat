c5ad9eac77304f6a31d0e26ed4ccc2c46a07f929
Satyamisme <satyamisme@gmail.com>
2021-02-02 17:25:35 +0300

    Add files via upload

--- VersionDefine.bat
+++ VersionDefine.bat
@@ -1,15 +1,15 @@
 @rem set the upgrade ID
-set FILE_DLOAD_ID=BV7R2E6
+set FILE_DLOAD_ID=BV7R11HS
 
 @REM The REM product name is also defined here, in order to unify compatible products, set here, and the main line set set FILE_PRODUCT_NAME=M9625E or support CPIO package set FILE_PRODUCT_NAME=M9625E
 @REM Subsequent products like HWD14 need to be modified here to set FILE_PRODUCT_NAME=HWD14
-set FILE_PRODUCT_NAME=E5787s-33a
+set FILE_PRODUCT_NAME=E3372h-153
 
 @rem Set the software version number
-set SOFTWARE_VER=21.312.11.01.397
+set SOFTWARE_VER=21.318.01.00.393
 
 @rem Set the background version number
-set DASHBOARD_VER=WEBUI_17.100.14.02.397_NE5
+set DASHBOARD_VER=WEBUI_17.100.15.02.1006_NE5
 
 @rem Set the WEBUI version number
-set WEBUI_VER=WEBUI_17.100.14.02.397_NE5
+set WEBUI_VER=WEBUI_17.100.18.01.1133_NE5
